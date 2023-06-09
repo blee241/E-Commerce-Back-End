aec2ea434bffe2d69440c69f4d7ac2e3594505a8
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-09 15:41:07 -0700

    completed delete request for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,8 +64,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const data = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
