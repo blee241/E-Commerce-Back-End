462337cf7a496f80c764ef4480513e88b0962c32
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-09 14:59:51 -0700

    completed category put request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,8 +42,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const data = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
