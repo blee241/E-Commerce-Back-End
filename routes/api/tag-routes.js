03eb2cf0b39af84268d140b48e0f1839bedf8d42
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-09 15:33:27 -0700

    completed tag get req

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const data = await Tag.findOne({
+      include: [{ model: Product }],
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
